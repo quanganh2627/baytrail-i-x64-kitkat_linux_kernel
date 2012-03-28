0a080128e14431522a8227fcab1fa67a9ec65816
Lobo, Melwyn <melwyn.lobo@intel.com>
2012-03-28 11:33:14 +0530

    [PORT_FROM_R2]writeback: lessen sync_supers wakeup count
    BZ: 31185
    
    This patch is based similar optimization in R2 with commit
     7c08fc348676dfe32deb056a91bceec943db85e2
    
    The 'sync_supers' thread wakes up periodically (by default) and
    writes back all super blocks. It keeps waking up even if there
    are no dirty super-blocks. For many file-systems the superblock
    becomes dirty very rarely, if ever, so 'sync_supers' does not do
    anything most of the time.
    
    This patch improves 'sync_supers' and makes it sleep if all superblocks
    are clean and there is nothing to do. This helps saving the power.
    This optimization is important for small battery-powered devices.
    
    This patch was provided by Artem who developed while he was working
    for Nokia, and the patch has been put into Nokia's kernel based on
    what he told me  via private email.
    
    Change-Id: I93caca67f0f0d3b943b4fcba8b176a8f7ebd4da7
    Signed-off-by: Lobo, Melwyn <melwyn.lobo@intel.com>
    Signed-off-by: Winson Yung <winson.w.yung@intel.com>
    Signed-off-by: Artem Bityutskiy <dedekind1@gmail.com>
    Reviewed-on: http://android.intel.com:8080/41075
    Reviewed-by: Iyer, Sundar <sundar.iyer@intel.com>
    Reviewed-by: Gross, Mark <mark.gross@intel.com>
    Reviewed-by: Kuppuswamy, Sathyanarayanan <sathyanarayanan.kuppuswamy@intel.com>
    Reviewed-by: Martin, LoicX <loicx.martin@intel.com>
    Tested-by: Martin, LoicX <loicx.martin@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- fs/udf/super.c
+++ fs/udf/super.c
@@ -1976,7 +1976,7 @@ static int udf_fill_super(struct super_block *sb, void *options, int silent)
 	sb->s_op = &udf_sb_ops;
 	sb->s_export_op = &udf_export_ops;
 
-	sb->s_dirt = 0;
+	sb_mark_clean(sb);
 	sb->s_magic = UDF_SUPER_MAGIC;
 	sb->s_time_gran = 1000;
 
@@ -2103,7 +2103,7 @@ static void udf_error(struct super_block *sb, const char *function,
 
 	if (!(sb->s_flags & MS_RDONLY)) {
 		/* mark sb error */
-		sb->s_dirt = 1;
+		sb_mark_dirty(sb);
 	}
 	va_start(args, fmt);
 	vsnprintf(error_buf, sizeof(error_buf), fmt, args);
@@ -2159,7 +2159,7 @@ static int udf_sync_fs(struct super_block *sb, int wait)
 		 * the buffer for IO
 		 */
 		mark_buffer_dirty(sbi->s_lvid_bh);
-		sb->s_dirt = 0;
+		sb_mark_clean(sb);
 		sbi->s_lvid_dirty = 0;
 	}
 	mutex_unlock(&sbi->s_alloc_mutex);
