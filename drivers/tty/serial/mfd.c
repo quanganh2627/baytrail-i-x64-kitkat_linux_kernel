f3432b4a54e04bb8af7d0d16d4355867888e94d6
lizhuangzhi <zhuangzhi.li@intel.com>
2013-04-07 16:25:41 +0800

    [PORT FROM R42L]HSU: Disable uart irq before switch IO from ALT to GPIO mode
    BZ: 96640
    
    When we use uart wake up funtion, the uart_rx will be switched from ALT function
    mode to GPIO input mode in runtime_suspend, if we don't disable uart irq before
    this aciton, there may be some signals disturb the uart_rx and cause the uart_rx
    interrupt triggered, So disable uart irq before switch action.
    
    Change-Id: Ifd366f117919914425897883786947894b87d2fc
    Signed-off-by: lizhuangzhi <zhuangzhi.li@intel.com>
    Reviewed-on: http://android.intel.com:8080/100343
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Tested-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/tty/serial/mfd.c
+++ drivers/tty/serial/mfd.c
@@ -2249,6 +2249,8 @@ static bool allow_for_suspend(struct uart_hsu_port *up)
 		return false;
 	}
 
+	disable_irq(up->port.irq);
+
 	if (up->running) {
 		if (up->dma_rx_on && dmarx_need_timer(up))
 			del_timer_sync(&up->rxc->rx_timer);
@@ -2308,6 +2310,7 @@ rx_dma_run:
 					jiffies + HSU_DMA_TIMEOUT_CHECK_FREQ);
 		}
 	}
+	enable_irq(up->port.irq);
 
 	return false;
 }
@@ -2381,8 +2384,6 @@ static int hsu_runtime_suspend(struct device *dev)
 	}
 #endif
 
-	disable_irq(up->port.irq);
-
 	if (up->index == logic_idx) {
 		struct uart_hsu_port *up3 = serial_hsu_ports[share_idx];
 		memcpy(up3->reg_shadow + 1, up3->port.membase + 1,
@@ -2410,7 +2411,6 @@ static int hsu_runtime_resume(struct device *dev)
 		}
 	}
 
-	enable_irq(up->port.irq);
 	if (up->dma_rx_on || dma_rx_on)
 		chan_writel(up->rxc, HSU_CH_CR, 0x3);
 
@@ -2426,6 +2426,7 @@ static int hsu_runtime_resume(struct device *dev)
 		if (up3->running)
 			intel_mid_hsu_resume(up3->index);
 	}
+	enable_irq(up->port.irq);
 
 	return 0;
 }
@@ -2483,7 +2484,6 @@ static int hsu_suspend(struct device *dev)
 		if (!allow_for_suspend(up))
 			return -EBUSY;
 
-		disable_irq(up->port.irq);
 		if (up->index == logic_idx) {
 			struct uart_hsu_port *up3 = serial_hsu_ports[share_idx];
 			if (up3->running) {
@@ -2514,9 +2514,6 @@ static int hsu_resume(struct device *dev)
 	&& (pdev->device != 0x1192)) {
 		up = priv;
 
-		if (up->suspended)
-			enable_irq(up->port.irq);
-
 		if (up->index == logic_idx) {
 			struct uart_hsu_port *up3 = serial_hsu_ports[share_idx];
 			if (up3->suspended && up3->running) {
@@ -2529,12 +2526,13 @@ static int hsu_resume(struct device *dev)
 		}
 
 		if (up->suspended && up->running) {
-			up->suspended = 0;
 			uart_resume_port(&serial_hsu_reg, &up->port);
 			intel_mid_hsu_resume(up->index);
 			schedule_work(&up->qwork);
-		} else
-			up->suspended = 0;
+		}
+		if (up->suspended)
+			enable_irq(up->port.irq);
+		up->suspended = 0;
 	}
 	return 0;
 }
