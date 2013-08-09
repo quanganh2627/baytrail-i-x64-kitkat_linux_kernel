c2383d8b0710ae9c3eac1b0170a64895eb42d181
buildslave <buildslave@buildbot.tl.intel.com>
2013-08-09 15:38:33 +0000

    Automatically revert commits from previous weekly release
    Change-Id: I1045546cb182039ef643bc37071dbd399940e622
    Signed-off-by: buildslave <buildslave@buildbot.tl.intel.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -15,7 +15,6 @@
 #include <linux/device.h>
 #include <linux/fault-inject.h>
 #include <linux/wakelock.h>
-#include <linux/pm_qos.h>
 
 #include <linux/mmc/core.h>
 #include <linux/mmc/pm.h>
@@ -373,7 +372,6 @@ struct mmc_host {
 #endif
 
 	struct mmc_panic_host *phost;
-	struct pm_qos_request *qos;
 	unsigned long		private[0] ____cacheline_aligned;
 };
 
