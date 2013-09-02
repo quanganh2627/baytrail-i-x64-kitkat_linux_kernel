1ede662c8a92254ca288eb553a5c86da50641859
Dong Chuanxiao <chuanxiao.dong@intel.com>
2013-09-02 18:16:41 +0800

    [PORT FROM R42B-STABLE]mmc: card: workaround for some 2+ cycles Ncrc timing SD
    BZ: 132257
    
    Some v3.0 SD card has 3~8 cycles Ncrc timing when DDR50
    enabled. While our current SD host controller only
    accepte fixed 2 cycles Ncrc timing for DDR50. This caused
    some SD card failed to work with DDR50 on our platform.
    Submit this patch to workaround this issue. It will
    just disable DDR50 mode and re-init the card with SDR25
    if this issue triggered.
    
    It also include the fix for bug#135808
    
    Org-Change-Id: I785cb72eaaf1b89828f6acf5534cd93fb60fc6a3
    Change-Id: I4c8c5376083f5871a72e0227da55f3047f0a4d25
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-on: http://android.intel.com:8080/128675
    Tested-by: Sun, Jianhua <jianhua.sun@intel.com>
    Reviewed-by: Tang, Richard <richard.tang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -286,6 +286,7 @@ struct mmc_host {
 #define MMC_CAP2_LED_SUPPORT	(1 << 13)	/* led support */
 #define MMC_CAP2_PWCTRL_POWER	(1 << 14)	/* power control card power */
 #define MMC_CAP2_FIXED_NCRC	(1 << 15)	/* Fixed 2 cycles Ncrc */
+#define MMC_CAP2_PWOFF_DELAY	(1 << 16)	/* delay after power off */
 
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 
