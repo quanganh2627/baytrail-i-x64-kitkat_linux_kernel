a693266d4aec7e74f136355a78ca08fbc208c23d
Jet Chen <jet.chen@intel.com>
2013-09-26 18:25:12 +0800

    cts: change file permission about hsic in /sys
    BZ:134861
    
    This is the requirement by cts test
    Files in /sys can't be writable to everyone
    
    Change-Id: I63f10ee7439cd3c5e052d5f27077c99480a22b64
    Signed-off-by: Jet Chen <jet.chen@intel.com>
    Signed-off-by: feng wang <feng.a.wang@intel.com>
    Reviewed-on: http://android.intel.com:8080/134195
    Reviewed-by: Wang, Yu Y <yu.y.wang@intel.com>
    Tested-by: Wang, Ran <ran.wang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/usb/host/ehci-tangier-hsic-pci.c
+++ drivers/usb/host/ehci-tangier-hsic-pci.c
@@ -458,7 +458,7 @@ static ssize_t hsic_host_resume_store(struct device *dev,
 	return -EINVAL;
 }
 
-static DEVICE_ATTR(host_resume, S_IRUGO | S_IWUSR | S_IROTH | S_IWOTH,
+static DEVICE_ATTR(host_resume, S_IRUGO | S_IWUSR | S_IROTH,
 		NULL, hsic_host_resume_store);
 
 static ssize_t hsic_port_enable_show(struct device *dev,
@@ -523,7 +523,7 @@ static ssize_t hsic_port_enable_store(struct device *dev,
 	return size;
 }
 
-static DEVICE_ATTR(hsic_enable, S_IRUGO | S_IWUSR | S_IROTH | S_IWOTH,
+static DEVICE_ATTR(hsic_enable, S_IRUGO | S_IWUSR | S_IROTH,
 		hsic_port_enable_show, hsic_port_enable_store);
 
 static ssize_t hsic_port_inactivityDuration_show(struct device *dev,
@@ -561,7 +561,7 @@ static ssize_t hsic_port_inactivityDuration_store(struct device *dev,
 }
 
 static DEVICE_ATTR(L2_inactivityDuration,
-		S_IRUGO | S_IWUSR | S_IROTH | S_IWOTH,
+		S_IRUGO | S_IWUSR | S_IROTH,
 		hsic_port_inactivityDuration_show,
 		 hsic_port_inactivityDuration_store);
 
@@ -606,7 +606,7 @@ static ssize_t hsic_autosuspend_enable_store(struct device *dev,
 	return size;
 }
 
-static DEVICE_ATTR(L2_autosuspend_enable, S_IRUGO | S_IWUSR | S_IROTH | S_IWOTH,
+static DEVICE_ATTR(L2_autosuspend_enable, S_IRUGO | S_IWUSR | S_IROTH,
 		hsic_autosuspend_enable_show,
 		 hsic_autosuspend_enable_store);
 
@@ -644,7 +644,7 @@ static ssize_t hsic_bus_inactivityDuration_store(struct device *dev,
 }
 
 static DEVICE_ATTR(bus_inactivityDuration,
-		S_IRUGO | S_IWUSR | S_IROTH | S_IWOTH,
+		S_IRUGO | S_IWUSR | S_IROTH,
 		hsic_bus_inactivityDuration_show,
 		 hsic_bus_inactivityDuration_store);
 
@@ -691,7 +691,7 @@ static ssize_t hsic_remoteWakeup_store(struct device *dev,
 	return size;
 }
 
-static DEVICE_ATTR(remoteWakeup, S_IRUGO | S_IWUSR | S_IROTH | S_IWOTH,
+static DEVICE_ATTR(remoteWakeup, S_IRUGO | S_IWUSR | S_IROTH,
 		hsic_remoteWakeup_show, hsic_remoteWakeup_store);
 
 static ssize_t
