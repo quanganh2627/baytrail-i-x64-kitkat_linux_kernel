0f8580d0f3d2b6e32b0af996c7449c0df58eff8a
Liu, Chuansheng <chuansheng.liu@intel.com>
2013-11-05 13:20:02 -0800

    [PORT FROM MAIN] printk: deadlock when calling printk in nmi handle
    BZ: 144505
    
    Current printk implementation can not fully support that
    calling it in nmi handler for SMP arch.
    
    There is typical case in nmi handler function arch_trigger_all_cpu_backtrace
    
    In my platform, there are 2 CPUs, when function arch_trigger_all_cpu_backtra
    is called, 2 CPUs will recevied the nmi interrupts, and the
    arch_trigger_all_cpu_backtrace_handler() will called on 2 CPUs:
    
    case1:
    CPU0                                            CPU1
    calling arch_trigger_all_cpu_backtrace()        calling printk, and has obta
    nmi interrupt received                          nmi interrupt received
    call arch_trigger_all_cpu_backtrace_handler()   call arch_trigger_all_cpu_ba
    Obtain arch_spin_lock(&lock);                   Waiting for arch_spin_lock(&
    Continue to call printk()
    CPU0 will be blocked by logbuf_lock             CPU1 is blocked by arch_spin
    
    The deadlock will be happening.
    
    case2:
    CPU0                                             CPU1:(run dmesg command)
    calling arch_trigger_all_cpu_backtrace()         calling do_syslog
                                                     Obtaining the logbuf_lock
    nmi interrupt received                           nmi interrupt received
    ....
    The dealock will happen also somtimes.
    
    I just write a simple interface to run the arch_trigger_all_cpu_backtrace_ha
    it will trigger dead lock many times.
    
    The solution is when printk is called in nmi handler, we will use trylock in
    And in nmi handler, do the call the console write function because normal co
    include many spin locks also. This fix can confirm the traces in nmi handler
    almost.
    
    This fix is posted in the LKML also https://lkml.org/lkml/2012/7/4/273.
    
    Change-Id: Icbefaaa904b59002d22476de65c19e4ed3c626cc
    Orig-Change-Id: I76ea71dd4b54048e9bcf5435678c326876c41c31
    Signed-off-by: Liu, Chuansheng <chuansheng.liu@intel.com>

--- kernel/printk.c
+++ kernel/printk.c
@@ -859,7 +859,7 @@ static int console_trylock_for_printk(unsigned int cpu)
 {
 	int retval = 0, wake = 0;
 
-	if (console_trylock()) {
+	if (!in_nmi() && console_trylock()) {
 		retval = 1;
 
 		/*
@@ -936,7 +936,11 @@ asmlinkage int vprintk(const char *fmt, va_list args)
 	}
 
 	lockdep_off();
-	raw_spin_lock(&logbuf_lock);
+	if (unlikely(in_nmi())) {
+		if (!raw_spin_trylock(&logbuf_lock))
+			goto out_restore_lockdep_irqs;
+	} else
+		raw_spin_lock(&logbuf_lock);
 	printk_cpu = this_cpu;
 
 	if (recursion_bug) {
@@ -1036,6 +1040,7 @@ asmlinkage int vprintk(const char *fmt, va_list args)
 	if (console_trylock_for_printk(this_cpu))
 		console_unlock();
 
+out_restore_lockdep_irqs:
 	lockdep_on();
 out_restore_irqs:
 	local_irq_restore(flags);
