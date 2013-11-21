dd1ee8039910b38febdd9cd75d4a70886437a6ea
Andrew Boie <andrew.p.boie@intel.com>
2013-11-20 17:39:06 -0800

    add x509keyid tool
    This tool is required by sign-file in this older implementation.
    
    Change-Id: Icacd39fc5156ad162bccadcf7e3a5ee2a0ecc93b
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,10 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := sign-file
 LOCAL_MODULE_TAGS := optional
 LOCAL_PREBUILT_EXECUTABLES := scripts/sign-file
+include $(BUILD_HOST_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := x509keyid
+LOCAL_MODULE_TAGS := optional
+LOCAL_PREBUILT_EXECUTABLES := scripts/x509keyid
 include $(BUILD_HOST_PREBUILT)
 
