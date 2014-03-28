790f5228a34aa391e567f23f5f258db64520a7c2
Pallavi <pallavi.g@intel.com>
2014-03-28 22:14:17 +0530

    drm/i915 Added panel fitter scaling src size prop
    BZ: 185169
    
    Added new scaling src size property.
    
    Added support to change the panel fitter mode withoout modeset
    if the panel fitter is already enabled.
    
    Made the Source set size property independent of panel fitter
    property and now we can control the source size on the fly
    irrespective of the panel fitter mode.
    
    Change-Id: I13c4af76b0ab8f1edce7cb2dbe14c7f37f28934a
    Signed-off-by: Pallavi <pallavi.g@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1559,6 +1559,7 @@ typedef struct drm_i915_private {
 	struct drm_property *broadcast_rgb_property;
 	struct drm_property *force_audio_property;
 	struct drm_property *force_pfit_property;
+	struct drm_property *scaling_src_size_property;
 
 	bool hw_contexts_disabled;
 	uint32_t hw_context_size;
