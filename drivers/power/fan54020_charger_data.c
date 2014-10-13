a81ee669889e3e5642cfaecfc3b5bf2caff57e6e
Li, Ning <ning.li@intel.com>
2014-10-13 10:31:28 +0800

    FAN54015: Add Intel copyright header
    BZ: 39
    
    Add Intel copyright header.
    
    Change-Id: Ib12ef9261d9c9c82f92c664372006f359160ec82
    Signed-off-by: Li, Ning <ning.li@intel.com>

--- drivers/power/fan54020_charger_data.c
+++ drivers/power/fan54020_charger_data.c
@@ -1,3 +1,18 @@
+/*
+ * -------------------------------------------------------------------------
+ *  Copyright (C) 2014 Intel Mobile Communications GmbH
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
 #include "fan54x_charger.h"
 
 #define REG_IC_INFO 0x00
