444d9b458006a4084081dcf742ce947c7dbc21ea
Mantesh Sarasetti <manteshx.sarasetti@intel.com>
2014-10-31 17:18:48 +0530

    Use kernel mode for firmware request
    Currently kernel is requsting firmware from userspace. With this
    approach, there is race between kernel and userspace
    for firmware request during suspend sequence resulting in delay in suspend.
    Fix is to use kernelmode instead of usermode to avoid these kind of
    race conditions.
    
    Change-Id: I3a2cf12a3a4b24f537aeb30975088882c617f94e
    Signed-off-by: Mantesh sarasetti <manteshx.sarasetti@intel.com>
    Suggested-by: Nemtsov, ArikX <arikx.nemtsov@intel.com>
    Reviewed-on: https://android.intel.com/295413
    Reviewed-by: sys_pccgand <sys_pccgand@intel.com>
    Reviewed-by: Sripathi, Srinivas <srinivas.sripathi@intel.com>
    Reviewed-by: Kandru, Suneel <suneel.kandru@intel.com>
    Tested-by: Kandru, Suneel <suneel.kandru@intel.com>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -260,7 +260,7 @@ static void fw_free_buf(struct firmware_buf *buf)
 /* direct firmware loading support */
 static char fw_path_para[256];
 static const char * const fw_path[] = {
-	fw_path_para,
+	"/etc/firmware",
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
