cdc0dd9b6b2d8646e3a05f7cc351af4cd7beee5e
Song, Jingshan <jingshan.song@intel.com>
2014-11-24 22:46:57 +0800

    Add function to catch BP offline log BZ: 320
    Change-Id: Ic5858204c05fdf51b1f0c492310ef695f41da6c9
    Signed-off-by: Song, Jingshan <jingshan.song@intel.com>

--- drivers/oct/oct.c
+++ drivers/oct/oct.c
@@ -39,6 +39,8 @@
 #include <linux/pm_runtime.h>		/*PM runtime*/
 #include <linux/of.h>
 
+#include <linux/syscalls.h>
+
 #include "oct_io.h"
 #include "oct.h"
 #include <linux/tadi.h>
@@ -87,6 +89,8 @@ static int major;
 static int minor;
 static struct class *class_oct;
 static struct file *fp;
+loff_t pos = 0;
+
 static struct task_struct *task;
 static wait_queue_head_t oct_wq, oct_poll_wq;
 static dma_addr_t phy_base_addr;
@@ -107,6 +111,28 @@ static unsigned int oct_read_ptr;
 static unsigned int data_available;
 struct device_pm_platdata *pm_platdata;
 
+// offline bplog definition
+#define OFFLOG_PATH_LEN 255
+
+#define OFFLOG_CONFIG_FILE "/system/etc/offlogcfg.txt"
+#define OFFLOG_CONFIG_FILE_SIZE 500
+
+#define OFFLOG_CONFIG_TAG_ONOFF "on_off"
+#define OFFLOG_CONFIG_TAG_PATH  "offlog_path"
+#define OFFLOG_CONFIG_TAG_SIZE  "offlog_size"
+#define OFFLOG_CONFIG_TAG_NUM   "offlog_number"
+
+#define OFFLOG_CONFIG_MAX_VALUE_LENGTH 50
+
+//#define DEFAULT_OCT_OFFLOG_SIZE 500000000
+#define DEFAULT_OCT_OFFLOG_SIZE 10000000
+#define DEFAULT_OCT_OFFLOG_NUMBER 5
+#define DEFAULT_OFFLOG_PATH "/data/logs/"
+#define DEFAULT_OFFLOG_NAME "bplog"
+static char oct_offlog_path[OFFLOG_PATH_LEN+1] = {0}; // /data/logs/bplog
+static unsigned int oct_offlog_onoff = 1;
+static unsigned int oct_offlog_size = DEFAULT_OCT_OFFLOG_SIZE;
+static unsigned int oct_offlog_number = DEFAULT_OCT_OFFLOG_NUMBER;
 /* power management section */
 
 static int pm_suspend_fct(struct device *dev)
@@ -281,6 +307,41 @@ void oct_set_raw_gadget(struct file *fp)
 	fp->f_op->unlocked_ioctl(fp, TCSETS, (unsigned long)&my_attr);
 }
 
+static void oct_detect_off_flag(char *gadget_name)
+{
+	struct file *fp = NULL;
+	mm_segment_t old_fs;
+	int retry = 3;
+	OCT_DBG("oct_detect_off_flag[%s]", gadget_name);
+
+	while (!kthread_should_stop()&&retry>=0) {
+		old_fs = get_fs();
+		set_fs(KERNEL_DS);
+		{
+			fp = filp_open(gadget_name, O_RDONLY, 0);
+		}
+		if (NULL == fp) {
+			OCT_LOG("NULL Gadget File Handle");
+			msleep(2000);
+			continue;
+		} else if (IS_ERR(fp)) {
+			OCT_LOG("Gadget fp=%x open failed!", (unsigned int)fp);
+			retry--;
+			msleep(2000);
+			continue;
+		} else {
+			OCT_LOG("Gadget %s open succeeded. fp=%x", gadget_name,(unsigned int)fp);
+			oct_out_path = OCT_PATH_FILE;
+            filp_close(fp, NULL);
+			break;
+		}
+
+		set_fs(old_fs);
+		set_current_state(TASK_INTERRUPTIBLE);
+	}
+	return;
+}
+
 static struct file *oct_open_gadget(char *gadget_name)
 {
 	struct file *fp = NULL;
@@ -291,7 +352,10 @@ static struct file *oct_open_gadget(char *gadget_name)
 		old_fs = get_fs();
 		set_fs(KERNEL_DS);
 		{
-			fp = filp_open(gadget_name, O_RDWR, 0);
+			if (oct_out_path == OCT_PATH_TTY)
+				fp = filp_open(gadget_name, O_RDWR, 0);
+			else if (oct_out_path == OCT_PATH_FILE)		
+				fp = filp_open(gadget_name, O_RDWR | O_CREAT | O_TRUNC, 0666);	
 		}
 		set_fs(old_fs);
 		if (NULL == fp) {
@@ -303,8 +367,9 @@ static struct file *oct_open_gadget(char *gadget_name)
 			msleep(2000);
 			continue;
 		} else {
-			OCT_LOG("Gadget %s open succeeded.", gadget_name);
-			oct_set_raw_gadget(fp);
+			OCT_LOG("Gadget %s open succeeded. fp=%x", gadget_name,(unsigned int)fp);
+			if (oct_out_path == OCT_PATH_TTY)
+				oct_set_raw_gadget(fp);
 			break;
 		}
 		set_current_state(TASK_INTERRUPTIBLE);
@@ -437,6 +502,242 @@ static void oct_hw_init(void)
 	return;
 }
 
+
+static int str2int(char* str){
+    int value = 0;
+    char *s = str;
+    if(NULL == str || strlen(str) == 0){
+         return 0;
+    }
+    do{
+        if(*s > '9' || *s < '0'){
+             continue;
+        }
+        value *= 10;
+        value += *s - '0';
+    }while(*(++s) != 0);
+
+    return value;
+
+}
+
+bool get_value_by_tag(char *config, char *tag, char *value, int size){
+    int i = 0;
+    char *tv = strstr(config, tag);
+    if(value == NULL || size==0){
+         return false;
+    }
+    if( tv != NULL && strlen(tv) != 0){
+        bool b_equal_symbol = false;
+        while(*(++tv) != '\n' && *tv != '\0' && i<size){
+            if( !((*tv >= 'a' && *tv <= 'z')
+                || (*tv >= 'A' && *tv <= 'Z')
+                || (*tv >= '0' && *tv <= '9')
+                || *tv == '_' || *tv == '.' || *tv == '=' || *tv == '/' || *tv == '\\')){
+                  continue;
+            }
+
+            if(b_equal_symbol){
+                value[i++] = *tv;
+            }
+
+            if(*tv == '='){
+                 b_equal_symbol = true;
+            }
+        }
+        value[i++] = '\0';
+    }
+    return true;
+}
+
+
+static bool oct_offlog_get_config(char* cfg_path){
+    char cfg_context[OFFLOG_CONFIG_FILE_SIZE+1] = {0};
+    char value_onoff[OFFLOG_CONFIG_MAX_VALUE_LENGTH+1] = {0};
+    char value_path[OFFLOG_CONFIG_MAX_VALUE_LENGTH+1] = {0};
+    char value_size[OFFLOG_CONFIG_MAX_VALUE_LENGTH+1] = {0};
+    char value_num[OFFLOG_CONFIG_MAX_VALUE_LENGTH+1] = {0};
+    struct file *fp_cfg;
+    loff_t pos_cfg = 0;
+    //loff_t pos_dir = 0;
+    mm_segment_t old_fs;
+    int file_len = 0;
+
+    OCT_LOG("oct_get_offlog_config");
+
+    old_fs = get_fs();
+    set_fs(KERNEL_DS);
+
+    fp_cfg = filp_open(cfg_path, O_RDONLY, 0);
+
+    if(!IS_ERR_OR_NULL(fp_cfg)){
+        file_len = fp_cfg->f_op->llseek(fp_cfg, 0, SEEK_END);
+        if( file_len > 0){
+            fp_cfg->f_op->read(fp_cfg, cfg_context, file_len, &pos_cfg);
+        }
+        if( strlen(cfg_context) != 0 ){
+            get_value_by_tag(cfg_context, OFFLOG_CONFIG_TAG_ONOFF, value_onoff, OFFLOG_CONFIG_MAX_VALUE_LENGTH);  
+            get_value_by_tag(cfg_context, OFFLOG_CONFIG_TAG_PATH, value_path, OFFLOG_CONFIG_MAX_VALUE_LENGTH);  
+            get_value_by_tag(cfg_context, OFFLOG_CONFIG_TAG_SIZE, value_size, OFFLOG_CONFIG_MAX_VALUE_LENGTH);  
+            get_value_by_tag(cfg_context, OFFLOG_CONFIG_TAG_NUM, value_num, OFFLOG_CONFIG_MAX_VALUE_LENGTH);  
+        }
+        filp_close(fp_cfg, NULL);
+        fp_cfg = NULL;
+    } else {
+        OCT_LOG("oct_get_offlog_config config not exist");
+    }
+	set_fs(old_fs);
+
+    // config onoff
+    oct_offlog_onoff = (value_onoff == NULL || strlen(value_onoff) == 0)? 0 : str2int(value_onoff);
+
+    // config path
+    memset (oct_offlog_path, 0, OFFLOG_PATH_LEN + 1);
+    if(value_path == NULL || strlen(value_path) == 0){
+        snprintf(oct_offlog_path, OFFLOG_PATH_LEN, "%s%s", DEFAULT_OFFLOG_PATH, DEFAULT_OFFLOG_NAME);
+    } else {
+        snprintf(oct_offlog_path, OFFLOG_PATH_LEN, "%s%s", value_path, DEFAULT_OFFLOG_NAME);
+    }
+
+    // config size
+    oct_offlog_size = str2int(value_size);
+    if(oct_offlog_size == 0){
+        oct_offlog_size = DEFAULT_OCT_OFFLOG_SIZE;
+    }
+
+    // config file numbers
+    oct_offlog_number = str2int(value_num);
+    if(oct_offlog_number == 0){
+        oct_offlog_number = DEFAULT_OCT_OFFLOG_NUMBER;
+    }
+
+    OCT_LOG("oct_offlog_get_config, onoff: %d, path: %s, size:%d, number:%d",oct_offlog_onoff, oct_offlog_path, oct_offlog_size, oct_offlog_number);
+
+    return true;
+}
+
+void oct_offlog_check_mk_dirs(void){
+     mm_segment_t old_fs;
+     int offlog_path_length = strlen(oct_offlog_path);
+     char c='/';
+     char *nextdir = NULL;
+
+     nextdir = strchr(oct_offlog_path+1, c);
+
+     old_fs = get_fs();
+     set_fs(KERNEL_DS);
+
+     while(NULL != nextdir && strlen(nextdir) != 0){
+         char curdir[OFFLOG_PATH_LEN+1] = {0};
+         //bool if_path_exist = false;
+
+         strncpy(curdir, oct_offlog_path, offlog_path_length - strlen(nextdir) +1);
+         // TODO: test if curdir exist. if not exist, create it.
+
+         sys_mkdir(curdir, 0777);
+         nextdir = strchr(nextdir+1, c);
+     }
+	 set_fs(old_fs);
+
+}
+
+
+static bool oct_offlog_if_rotate(void){
+    // if bplog exist
+    int err = 0;
+    if(NULL == oct_offlog_path){
+        OCT_LOG("oct_offlog_if_rotate offlog path null");
+        return false;
+    }
+    // TODO: check if bplog exist to deside if need rotate
+    /*
+    err = sys_access(oct_offlog_path, R_OK);
+    if( !err ){
+        OCT_LOG("oct_offlog_if_rotate offlog not exist");
+        return false;
+    }
+    */
+    OCT_LOG("oct_offlog_if_rotate return true");
+    return true;
+}
+
+static int oct_offlog_rotate_files(void)
+{
+    mm_segment_t old_fs;
+    int err;
+    int i;
+
+    OCT_LOG("oct_offlog_rotate_files");
+
+    //remove bplog.4.istp
+	old_fs = get_fs();
+	set_fs(KERNEL_DS);
+    {
+        char path_del[OFFLOG_PATH_LEN+1] = {0};
+        snprintf(path_del, OFFLOG_PATH_LEN, "%s.%d.istp", oct_offlog_path, oct_offlog_number-1);
+        err = sys_unlink(path_del);
+    }
+    //rename other files
+    for (i = oct_offlog_number-2; i >= 0; i--)
+    {
+        char oldpath[OFFLOG_PATH_LEN+1] = {0};
+        char newpath[OFFLOG_PATH_LEN+1] = {0};
+
+        if(i == 0){
+            snprintf(oldpath, OFFLOG_PATH_LEN, "%s", oct_offlog_path);
+        } else{
+            snprintf(oldpath, OFFLOG_PATH_LEN, "%s.%d.istp", oct_offlog_path, i);
+        }
+        snprintf(newpath, OFFLOG_PATH_LEN, "%s.%d.istp", oct_offlog_path, i+1);
+        sys_rename (oldpath, newpath);
+    }
+
+	set_fs(old_fs);
+
+    return 1;
+}
+
+static void oct_offlog_chat_cmd(const char *devname,char *atcmd)
+{
+    mm_segment_t oldfs;
+    struct file *filp;
+    unsigned int writed = 0;
+#define AT_CMD_LEN 64
+    char readbuf[AT_CMD_LEN] = {0};
+    unsigned int wait_ms= 200;
+    unsigned int max_wait_times = 5;
+    unsigned int wait_times = 0;
+
+
+    oldfs = get_fs();
+    set_fs(KERNEL_DS);
+
+    filp = filp_open(devname, O_RDWR, 0);
+
+    if (!filp || IS_ERR(filp)) {
+        OCT_LOG("can not open [%s],filp=%ld\n", devname, PTR_ERR(filp));
+    } else {
+        writed = filp->f_op->write(filp, atcmd, strlen(atcmd), &filp->f_pos);
+        OCT_LOG("write[%s][%d]: [%s]",devname,writed,atcmd);
+        do{
+            msleep(wait_ms);
+            filp->f_op->read(filp, readbuf, AT_CMD_LEN - 1, &filp->f_pos);
+            OCT_LOG("read[%s]: [%s]",devname, readbuf);
+        }while((strlen(readbuf) == 0) && (wait_times++ < max_wait_times));
+
+        filp_close(filp, NULL);
+    }
+    set_fs(oldfs);
+}
+
+static int oct_offlog_send_init_at( void ){
+    oct_offlog_chat_cmd("/dev/vbpipe14", "ATE0V1\r");
+    oct_offlog_chat_cmd("/dev/vbpipe14", "at+trace=1\r");
+    oct_offlog_chat_cmd("/dev/vbpipe14", "at+xsio=0\r");
+    oct_offlog_chat_cmd("/dev/vbpipe14", "at+xsystrace=0,\"bb_sw=1;3g_sw=1;digrf=1\",\"digrf=0x84;bb_sw=sdl:th,tr,st,db,pr,lt,li,gt,ae,mo\",\"oct=4\"\r");
+}
+
+
 static void oct_write_data_to_usb(void *ptr, int num_bytes)
 {
 	mm_segment_t old_fs;
@@ -506,14 +807,123 @@ static void oct_write_data_to_usb(void *ptr, int num_bytes)
 				    OCT_CNF_ENABLE_TRIG_CYCLE_INT_ENABLED);
 }
 
+static void oct_write_data_to_file(void *ptr, int num_bytes)
+{
+	mm_segment_t old_fs;
+	int written_bytes = 0;
+	int verify_fp = 2;
+	static int total_written_bytes = 0;
+
+	old_fs = get_fs();
+	set_fs(KERNEL_DS);
+    if(total_written_bytes >= oct_offlog_size){
+        if (!IS_ERR_OR_NULL(fp)){
+            filp_close(fp, NULL);
+            fp = NULL;
+            pos = 0;
+        }
+        oct_offlog_rotate_files();
+        total_written_bytes = 0;
+    }
+	while (verify_fp > 0) {
+
+		if (IS_ERR_OR_NULL(fp)) {
+			OCT_LOG("Gadget fp=%p is not valid=%ld!",
+					fp, PTR_ERR(fp));
+			/* re-open the tty gadget */
+			fp = filp_open(oct_offlog_path, O_RDWR | O_CREAT | O_TRUNC, 0666);
+		}
+		if (IS_ERR_OR_NULL(fp)) {
+			OCT_LOG("Gadget fp=%p re-open failed with error=%ld!",
+						fp, PTR_ERR(fp));
+			/* skip bytes in buffer and leave loop */
+			written_bytes = num_bytes;
+			verify_fp = -1;
+		} else {
+			/* try a first time write */
+
+			//OCT_LOG("Sending to File fp=%p @0x%x:0x%x ",fp,(char *)ptr, num_bytes);
+			written_bytes =
+				fp->f_op->write(fp, (char *)ptr, num_bytes, &pos);
+			if (written_bytes <= 0) {
+				OCT_LOG(
+					"Error: file write Error with valid fp=%p, %d",
+					fp, written_bytes);
+				/* come back and force the loop to re-open fp */
+				fp = NULL;
+				/* avoid infinity loop doing only
+				 * one time the loop */
+				verify_fp--;
+				/* skip bytes in buffer and leave loop */
+				written_bytes = num_bytes;
+			} else {
+				/* write sucessfuly to USB and leave the loop */
+				verify_fp = -1;
+                total_written_bytes += written_bytes;
+			}
+		}
+	}
+	set_fs(old_fs);
+
+	/* increment the read ptr anyhow to re-start timeout */
+	oct_read_ptr += written_bytes;
+	/* check if result is consistent */
+	if (oct_read_ptr > oct_ext_ring_buff_len) {
+		/* too many bytes */
+		OCT_LOG("Error: ReadPtr exceeds the buffer length");
+		OCT_LOG("written bytes 0x%x, oct_read_ptr 0x%x",
+				written_bytes, oct_read_ptr);
+		oct_read_ptr = 0;
+	} else if (oct_read_ptr == oct_ext_ring_buff_len) {
+		oct_read_ptr = 0; /*wrap around*/
+		OCT_DBG("ReadPtr wrap around:");
+		OCT_DBG(" written bytes 0x%x, oct_read_ptr 0x%x",
+				written_bytes, oct_read_ptr);
+	}
+	SET_OCT_OCT_SW_RPTR(oct_trform, oct_read_ptr);
+	data_available = 0;
+	/* reset & enable CYCLE interrupt */
+	SET_OCT_OCT_MASTER_TRIG_CYCLE_DMA_TRIG_CYCLE(oct_trform,
+							   current_timeout);
+	SET_OCT_OCT_CNF_ENABLE_TRIG_CYCLE_INT(oct_trform,
+				    OCT_CNF_ENABLE_TRIG_CYCLE_INT_ENABLED);
+}
+
+
+static const struct file_operations oct_proc_fops = {
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= seq_release,
+};
+
 static int oct_thread(void *param)
 {
 	unsigned int num_bytes = 0, wptr_un = 0;
 	OCT_DBG("Enter thread");
 
-	/* if (oct_out_path == OCT_PATH_TTY) */
-	fp = oct_open_gadget(USB_CH_NAME);
+	oct_detect_off_flag(OFFLOG_CONFIG_FILE);
+    OCT_DBG("oct_detect_off_flag oct_out_path: %d", oct_out_path);
 
+	if (oct_out_path == OCT_PATH_FILE)	{
+        oct_offlog_get_config(OFFLOG_CONFIG_FILE);
+        if(oct_offlog_onoff == 0){
+            oct_out_path = DEFAULT_OCT_PATH;
+        }
+    }
+    OCT_DBG("oct_detect_off_flag oct_out_path: %d", oct_out_path);
+    OCT_LOG("oct_out_path %d",oct_out_path);
+
+
+	if (oct_out_path == OCT_PATH_TTY)
+		fp = oct_open_gadget(USB_CH_NAME);
+	else if (oct_out_path == OCT_PATH_FILE)	{
+        oct_offlog_check_mk_dirs();
+        if(oct_offlog_if_rotate()){
+            oct_offlog_rotate_files();
+        }
+        oct_offlog_send_init_at();
+		fp = oct_open_gadget(oct_offlog_path);
+    }
 	while (!kthread_should_stop()) {
 		wait_event_interruptible(oct_wq, oct_events);
 		oct_events = 0;
@@ -549,10 +959,21 @@ static int oct_thread(void *param)
 					DMA_FROM_DEVICE);
 
 				/* call subscribed function to forward data */
+/*
 				oct_write_data_to_usb((char *)
-				(&((char *)oct_ext_rbuff_ptr)[oct_read_ptr]),
-					num_bytes);
-				OCT_DBG("Sent out %d bytes", num_bytes);
+					(&((char *)oct_ext_rbuff_ptr)[oct_read_ptr]),
+						num_bytes);
+*/
+//					OCT_DBG("Sent out %d bytes", num_bytes);
+
+			}
+			else if (oct_out_path == OCT_PATH_FILE)
+			{
+				/* call subscribed function to forward data */
+					oct_write_data_to_file((char *)
+					(&((char *)oct_ext_rbuff_ptr)[oct_read_ptr]),
+						num_bytes);
+//					OCT_DBG("Sent out %d bytes", num_bytes);
 			}
 		} else /* if no data available*/
 			OCT_DBG("NO data available to be sent out");
