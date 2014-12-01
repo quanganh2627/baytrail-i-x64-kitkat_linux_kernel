087e8fe90894a24b0a9b97954cd3f2d8e8cda9b5
Min Le <min.le@intel.com>
2014-12-01 17:16:59 +0800

    add board_id for sf3g svb2 and mrd7s
    BZ: 97
    
    Change-Id: Icb87b6f1a9f55aa8b798ae2ba6b9a06b5342c0da
    Signed-off-by: Min Le <min.le@intel.com>

--- include/sofia/board_id.h
+++ include/sofia/board_id.h
@@ -2,9 +2,9 @@
 #define _BOARD_ID_H
 
 #define BOARD_SOFIA3G_SVB1
-#define BOARD_SOFIA3G_SVB2
+#define BOARD_SOFIA3G_SVB2		(1536)
 #define BOARD_SOFIA3G_MRD_P0
-#define BOARD_SOFIA3G_MRD_7S
+#define BOARD_SOFIA3G_MRD_7S		(15)
 #define BOARD_SOFIA3G_MRD_5S		(13)
 
 int sofia_board_is(id);
