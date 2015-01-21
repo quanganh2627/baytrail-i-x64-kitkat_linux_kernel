88bf18fa9217ac71e26022a989df8bfe1db6f606
Antoine Bouyer <antoine.bouyer@intel.com>
2015-01-21 13:26:50 +0100

    ASoC: xgold: prevent mutex in a spinlock
    BZ: 817
    Signed-off-by: Antoine Bouyer <antoine.bouyer@intel.com>
    Change-Id: Iec911d97ea6796863c92462d8e186519aadf9446
    Reviewed-on: https://opticm6.rds.intel.com/r/56649
    Reviewed-by: Xavier Roumegue <xavier.roumegue@intel.com>
    Tested-by: Xavier Roumegue <xavier.roumegue@intel.com>

--- sound/soc/xgold/xgold_pcm.c
+++ sound/soc/xgold/xgold_pcm.c
@@ -284,10 +284,11 @@ void xgold_dsp_hw_probe_a_handler(void *dev)
 	rw_shm_data.word_offset =
 		xgold_pcm->dsp->p_dsp_common_data->buf_sm_hw_probe_a_offset;
 
-	/* TODO: could we use same calculation as for playback and speech probes?
-		length = xrtd->stream->runtime->period_size *
-		xrtd->stream->runtime->channels;
-		rw_shm_data.len_in_bytes = length * 2;
+	/* TODO: could we use same calculation as for playback and
+	 * speech probes?
+	 *	length = xrtd->stream->runtime->period_size *
+	 *	xrtd->stream->runtime->channels;
+	 *	rw_shm_data.len_in_bytes = length * 2;
 	*/
 	rw_shm_data.len_in_bytes =
 		(2 * 240 * xrtd->stream->runtime->channels);
@@ -848,8 +849,7 @@ static int xgold_pcm_hw_free(struct snd_pcm_substream *substream)
 
 	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK &&
 		xgold_pcm != NULL && xgold_pcm->dma_mode) {
-		spin_lock_irqsave(&xrtd->lock, flags);
-
+		
 		if (xrtd->dmach) {
 			if (lpmp3_dma_release &&
 				STREAM_PLAY2 == xrtd->stream_type)
@@ -857,7 +857,9 @@ static int xgold_pcm_hw_free(struct snd_pcm_substream *substream)
 
 			/* request DMA shutdown */
 			xgold_debug("terminate all dma: %p\n", xrtd->dmach);
+			spin_lock_irqsave(&xrtd->lock, flags);
 			dmaengine_terminate_all(xrtd->dmach);
+			spin_unlock_irqrestore(&xrtd->lock, flags);
 
 			/* Release the DMA channel */
 			dma_release_channel(xrtd->dmach);
@@ -866,7 +868,6 @@ static int xgold_pcm_hw_free(struct snd_pcm_substream *substream)
 
 		/* Free scatter list memory*/
 		kfree(xrtd->dma_sgl);
-		spin_unlock_irqrestore(&xrtd->lock, flags);
 	}
 
 	/* Free DMA buffer */
