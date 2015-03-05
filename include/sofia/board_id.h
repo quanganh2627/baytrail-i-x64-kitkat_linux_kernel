a32423914dce0a0640408f0efe843a7e9bd4e0c9
Huajun Li <huajun.li@intel.com>
2015-03-05 11:09:45 +0800

    Support MRD 5S Quad Band variant
    [BYPASS_BZ_CHECK]
    
    MRD 5S Quad Band variant is same with other MRD 5S except for RF
    component and HWID, so add MRD 5S Quad Band support.
    
    Change-Id: Iba1c9c1081d2ff7cf59ea7f351ebf91c643dfc3f
    Signed-off-by: Huajun Li <huajun.li@intel.com>

--- include/sofia/board_id.h
+++ include/sofia/board_id.h
@@ -6,6 +6,10 @@
 #define BOARD_SOFIA3G_MRD_P0
 #define BOARD_SOFIA3G_MRD_7S		(15)
 #define BOARD_SOFIA3G_MRD_5S		(13)
+/* MRD 5S quad band variant has different RF,
+ * other components are same
+ */
+#define BOARD_SOFIA3G_MRD_5S_QB		(6)
 
 int sofia_board_is(unsigned int id);
 unsigned int sofia_get_board_id(void);
